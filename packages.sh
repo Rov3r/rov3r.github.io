c258195a9cc57f930424af1fac50e4f0acc1c179
Rov3r <jaredgardner500@gmail.com>
2018-02-27 14:56:37 -0600

    added SCOthman to my repo

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/R3F1ND debs
-dpkg-deb -bZgzip projects/SCOthman for Snapchat debs
+dpkg-deb -bZgzip projects/SCOthmanForSnapchat debs
 # dpkg-deb -bZgzip projects/<packagename> <output folder>
