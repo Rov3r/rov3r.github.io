52b1faa1e29231c4f16d0dfad7bf2b72dff6f995
Rov3r <jaredgardner500@gmail.com>
2018-02-27 14:45:29 -0600

    added SCOthman for Snapchat for an older version

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/R3F1ND debs
-dpkg-deb -bZgzip projects/Clutch (iOS 6) debs
+dpkg-deb -bZgzip projects/SCOthman for Snapchat debs
 # dpkg-deb -bZgzip projects/<packagename> <output folder>
